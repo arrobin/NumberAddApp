93f284465cc5ca8f4b724d76430cbe14605fc32b
arrobin <ashiqur007@yahoo.com>
2015-05-01 19:15:18 +0600

    This App Can Add Numbers

---
+++ NumberApp/Number.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberApp
+{
+    public partial class Number : Form
+    {
+        public Number()
+        {
+            InitializeComponent();
+        }
+
+        private void numberListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            double selecetedItem = Convert.ToDouble(numberListBox.SelectedIndex);
+            MessageBox.Show("Seleceted Number is "+selecetedItem.ToString());
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            double number = Convert.ToDouble(numberTextBox.Text);
+
+            for (int i = 0; i <= number; i++)
+            {
+                numberListBox.Items.Add(i);
+            }
+        }
+    }
+}
